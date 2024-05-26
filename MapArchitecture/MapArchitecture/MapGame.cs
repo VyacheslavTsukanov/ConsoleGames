6b54b4c6f4d7ac8d3710f5e13942fc5a62c8b285
git config --global user.name <zstarenky@yandex.ru>
2024-05-26 22:22:18 +0400

    Map created

---
+++ MapArchitecture/MapArchitecture/MapGame.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace MapArchitecture
+{
+    class MapGame
+    {
+        private char[,] map;
+        private int mapWidth;
+        private int mapHeight;
+        private int playerX;
+        private int playerY;
+
+        public MapGame(int width, int height)
+        {
+            mapWidth = width;
+            mapHeight = height;
+            map = new char[mapWidth, mapHeight];
+            playerX = 0;
+            playerY = 0;
+        }
+
+        public void InitializeMap()
+        {
+            for (int i = 0; i < mapWidth; i++)
+            {
+                for (int j = 0; j < mapHeight; j++)
+                {
+                    map[i, j] = '.';
+                }
+            }
+        }
+
+        public void DrawMap()
+        {
+            Console.Clear();
+            for (int i = 0; i < mapWidth; i++)
+            {
+                for (int j = 0; j < mapHeight; j++)
+                {
+                    if (i == playerX && j == playerY)
+                    {
+                        Console.Write('P');
+                    }
+                    else
+                    {
+                        Console.Write(map[i, j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void MovePlayer(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.UpArrow:
+                    if (playerX > 0)
+                    {
+                        playerX--;
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (playerX < mapWidth - 1)
+                    {
+                        playerX++;
+                    }
+                    break;
+                case ConsoleKey.LeftArrow:
+                    if (playerY > 0)
+                    {
+                        playerY--;
+                    }
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (playerY < mapHeight - 1)
+                    {
+                        playerY++;
+                    }
+                    break;
+            }
+        }
+    }
+}
--- MapArchitecture/MapArchitecture/Program.cs
+++ MapArchitecture/MapArchitecture/Program.cs
@@ -6,7 +6,16 @@ namespace MapArchitecture
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MapGame gameMap = new MapGame(10, 10);
+            gameMap.InitializeMap();
+
+            while (true)
+            {
+                gameMap.DrawMap();
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                gameMap.MovePlayer(keyInfo.Key);
+            }
         }
     }
 }
